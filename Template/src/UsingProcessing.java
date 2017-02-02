a6dbacb1bbafd2ecb916720dddc9ce1649390cc0
ElCodigoAmigo <ElCodigoAmigo@gmail.com>
2017-02-02 12:47:34 +0100

    Añadido plantilla para usar processing con eclipse
    Se ha creado una plantilla para facilitar la creación de proyectos que requieran de processing y ,sobre todo, para que sirva de base para programar a lo largo de los vídeos de https://www.youtube.com/channel/UCL0jBlED8RjFQrsGvGGYXoA

---
+++ Template/src/UsingProcessing.java
@@ -0,0 +1,22 @@
+import processing.core.PApplet;
+
+public class UsingProcessing extends PApplet{
+	
+	public void settings() {
+
+	}
+
+	public void setup() {
+		
+	}
+
+	public void draw() {
+			
+	}
+
+	public static void main(String[] args) {
+		PApplet.main("UsingProcessing");
+	}
+	
+	
+}
