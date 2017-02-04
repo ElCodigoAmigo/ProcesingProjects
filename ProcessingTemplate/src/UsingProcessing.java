6c6792c593fd8e013b57fc2a0ed16d26fea6d204
ElCodigoAmigo <ElCodigoAmigo@gmail.com>
2017-02-04 16:00:44 +0100

    Arreglado Problema de importacion de template

---
+++ ProcessingTemplate/src/UsingProcessing.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+
+public class UsingProcessing extends PApplet {
+	
+	
+	public void settings() {
+		size(500, 450);
+	}
+
+	public void setup() {
+				
+	}
+
+	public void draw() {
+		
+	}
+
+	public static void main(String[] args) {
+		PApplet.main("UsingProcessing");
+	}
+	
+	
+}
